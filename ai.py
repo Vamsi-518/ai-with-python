08ca1b7b3178c83167a9c15b8c2f2495cf6eb039
vamsi <chmnvkrishna@gmail.com>
2026-06-16 17:51:27 +0530

    ai.py

---
+++ ai.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+
+X = np.array([[1], [2], [3], [4]])
+y = np.array([2, 4, 6, 8])
+
+model = LinearRegression()
+model.fit(X, y)
+
+prediction = model.predict([[5]])
+print(prediction)
